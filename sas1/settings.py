503b1686332e19fe7ef4e26d94ea2aa86b183600
Palanga123 <palangageorgen@gmail.com>
2024-06-12 10:34:52 +0200

    new changes

--- sas1/settings.py
+++ sas1/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
